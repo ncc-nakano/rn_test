55f3ccc2cb756da043bfc3ea98638558b0fcefd5
ncc-nakano <93506589+ncc-nakano@users.noreply.github.com>
2021-11-26 10:45:32 +0900

    test

--- App.js
+++ App.js
@@ -1,16 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends React.Component {
+  name = "hoge"
+  state = {name:"hoge"}
+
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!!</Text>
+        <Text>{this.state.name}</Text>
+        <Button
+            title="click here" onPress={this.thanksAlert}
+        />
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
+  
+  // function
+  thanksAlert = () => {
+    alert("thanks!");
+    console.log("thanks is pushed");
+    this.setState({name:"fuga"});
+  }
+
 }
 
+
+// style
 const styles = StyleSheet.create({
   container: {
     flex: 1,
